492027fa0dd4d37712d6210a99873d14d259f612
satilpereira <satil.prog@gmail.com>
2024-02-11 14:55:48 -0300

    add strings.cpp

---
+++ foundamentals/strings/strings.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(void)
+{
+  // Strings are sequences of characters
+  // In C++, strings are objects of the class std::string
+  std::string str = "Hello, World!";
+  std::cout << str << std::endl;
+
+  return 0;
+}
