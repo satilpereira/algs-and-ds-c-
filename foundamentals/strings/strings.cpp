8ab16355fdb4ee7a0896d56f794ed951d1b19351
satilpereira <satil.prog@gmail.com>
2024-02-12 14:56:58 -0300

    update strings.cpp to add lenght

--- foundamentals/strings/strings.cpp
+++ foundamentals/strings/strings.cpp
@@ -5,7 +5,12 @@ int main(void)
   // Strings are sequences of characters
   // In C++, strings are objects of the class std::string
   std::string str = "Hello, World!";
-  std::cout << str << std::endl;
+  std::cout << str << std::endl
+            << std::endl;
+
+  // To get the length of a string, use the length() method
+  std::cout << "The length of the string is " << str.length() << std::endl
+            << std::endl;
 
   return 0;
 }
